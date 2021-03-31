6a7a4943676d56cd0a86b8a24782f1e21438115f
Dorfami950 <dorfam.omidi@email.kpu.ca>
2021-03-31 10:39:30 -0700

    game-logic-2

--- app/src/main/java/com/game/rememberwhen/Score.java
+++ app/src/main/java/com/game/rememberwhen/Score.java
@@ -1,10 +1,20 @@
 package com.game.rememberwhen;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Score {
+    TextView playersListText;
+    RecyclerView players_list_view;
     Player player;
     //here answer means if storyteller telols truth or lie
     String answer;
@@ -14,64 +24,67 @@ public class Score {
     String respond;
     public String getRespond() { return respond; }
 
-    //assign the score for all players at one round
-    //should be call at end of each cycle
     public Score(List<Player> playerList) {
         for (int i = 0; i < playerList.size(); i++) {
-            if(playerList.get(i).score==500)
-            {
-                //assign code for saying winner is found
+            if (playerList.get(i).score == 500) {
+                  playersListText.setText("Winner found");
             }
+
             if (playerList.get(i).getStatus() == "storyTeller") {
                 if (answer.equals("truth")) {
-                       switch (respond) {
-                            case "truth":
-                            player.setScore(player.getScore()+ playerList.size() * 10); break;
+                    switch (respond) {
+                        case "truth":
+                            player.setScore(player.getScore() + playerList.size() * 10);
+                            break;
 
-                            case "MakeItUp":
-                            // what happen?
+                        case "MakeItUp":
+                            player.setScore(player.getScore() - 10);
                             break;
 
 
-                        }
+                    }
+                } else {
+                    switch (respond) {
+                        case "truth":
+                            player.setScore(player.getScore() + playerList.size() * 20);
+                            break;
+
+                        case "MakeItUp":
+                            player.setScore(player.getScore() - 20);
+                            break;
+
+                    }
                 }
-                 else{
-                        switch (respond) {
-                            case "truth":
-                            player.setScore(player.getScore()+ playerList.size() * 20); break;
 
-                            case "MakeItUp":
-                            player.setScore(player.getScore()-20); break;
-
-                        }
-                 }
-
-            }
-            else{
-                if(respond.equals(""))
-                    player.setScore(player.getScore()- 10);
+            } else {
+                if (respond.equals(""))
+                    player.setScore(player.getScore() - 10);
                 else if (answer.equals("truth")) {
-                        switch (respond) {
-                            case "truth":
-                            player.setScore(player.getScore()+ 10); break;
+                    switch (respond) {
+                        case "truth":
+                            player.setScore(player.getScore() + 10);
+                            break;
 
-                            case "MakeItUp":
-                            player.setScore(player.getScore()- 10); break;
+                        case "MakeItUp":
+                            player.setScore(player.getScore() - 10);
+                            break;
 
-                        }
-                }
-                else{
-                     switch (respond) {
-                           case "truth":
-                            player.setScore(player.getScore()-5); break;
+                    }
+                } else {
+                    switch (respond) {
+                        case "truth":
+                            player.setScore(player.getScore() - 5);
+                            break;
 
-                            case "MakeItUp":
-                            player.setScore(player.getScore()+10); break;
-                     }
+                        case "MakeItUp":
+                            player.setScore(player.getScore() + 10);
+                            break;
+                    }
                 }
             }
         }
+    }
 
    }
-}
+
 
